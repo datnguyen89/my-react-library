56a9a517d561f3b824b1735eb1d3fe59ca70f218
datnt <ntdat89.hn@gmail.com>
2024-01-14 21:10:20 +0700

    .

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 import React from 'react'
 import styles from './styles.module.css'
+import { Button, Input } from 'antd'
+import { UserOutlined } from '@ant-design/icons'
 
 export const ExampleComponent = ({ text }) => {
-  return <div className={styles.test}>Example Component: {text}</div>
+  return (
+    <div className={styles.test}>
+      Example Component: {text}
+      <Button type='primary'>Test Antd</Button>
+      <Input size='large' placeholder='large size' prefix={<UserOutlined />} />
+    </div>
+  )
 }
